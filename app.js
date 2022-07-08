e40b3d9deacfcb9956a9a76dac144df28d2909d5
Asad Mahmood <asadmhmd1234@gmail.com>
2022-07-08 23:02:23 +0530

    Add dependencies required for heroku

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -13,8 +14,7 @@ var helmet = require("helmet");
 
 var app = express();
 const mongoose = require("mongoose");
-const mongoDBUri =
-  "mongodb+srv://library:library-tutorial@library.3jvvf.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDBUri = process.env.MONGODB_URI;
 
 mongoose.connect(mongoDBUri, {
   useNewUrlParser: true,
@@ -27,7 +27,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use(helmet);
+app.use(helmet());
 app.use(compression());
 app.use(logger("dev"));
 app.use(express.json());

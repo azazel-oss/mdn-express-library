5b79a7d24460ee47ca9297f3a60f73e96807abcc
Asad Mahmood <asadmhmd1234@gmail.com>
2022-07-08 22:50:27 +0530

    Make the application production ready

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
+var compression = require("compression");
+var helmet = require("helmet");
+
 var app = express();
 const mongoose = require("mongoose");
 const mongoDBUri =
@@ -24,6 +27,8 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(helmet);
+app.use(compression());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
